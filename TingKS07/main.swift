51d60623eff8797e83b43dc2ea62fb09050d7dcf
user <user@userde-MacBook-2.local>
2017-06-13 09:49:48 +0800

    V2

--- TingKS07/main.swift
+++ TingKS07/main.swift
@@ -8,12 +8,25 @@
 
 import Foundation
 
-var str = "Hello, World"
+// Regex
+//var str = "Hello, World"
+//
+//if let range = "Hello, World".range(of: "^H.*d$", options: .regularExpression){
+//    let result = "Hello, World".substring(with: range)
+//    print("OK: \(result)")
+//}else{
+//    print("XX")
+//}
 
-if let range = "Hello, World".range(of: "^H.*d$", options: .regularExpression){
-    let result = "Hello, World".substring(with: range)
-    print("OK: \(result)")
-}else{
-    print("XX")
+// 閉包1
+func mysort(a:Int, b:Int) -> Bool{
+    //print("\(a) : \(b)")
+    return a<b
 }
+
+print(type(of:mysort))
+
+let a1 = [4,2,7,1,45,1234,3,14,5]
+let a2 = a1.sorted(by: mysort)
+print(a2.description)
 
