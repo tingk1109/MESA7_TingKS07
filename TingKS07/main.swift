7877952cd22352f616036e4ea5b1810c45ac9fb6
user <user@userde-MacBook-2.local>
2017-06-13 10:04:24 +0800

    V3

--- TingKS07/main.swift
+++ TingKS07/main.swift
@@ -18,7 +18,7 @@ import Foundation
 //    print("XX")
 //}
 
-// 閉包1
+// 源頭0
 func mysort(a:Int, b:Int) -> Bool{
     //print("\(a) : \(b)")
     return a<b
@@ -26,7 +26,33 @@ func mysort(a:Int, b:Int) -> Bool{
 
 print(type(of:mysort))
 
-let a1 = [4,2,7,1,45,1234,3,14,5]
+let a1 = [4,2,7,1]
 let a2 = a1.sorted(by: mysort)
 print(a2.description)
+
+print("------")
+// 閉包1
+let a3 = a1.sorted(by:
+    {(a,b) -> Bool in
+        //print("\(a) : \(b)")
+        return a < b
+    })
+print(a3.description)
+
+print("------")
+// 閉包2
+let a4 = a1.sorted(by:{(a,b) -> Bool in a < b})
+print(a4.description)
+
+print("------")
+// 閉包3
+let a5 = a1.sorted(by:{$0 < $1})
+print(a5.description)
+
+print("------")
+// 閉包4
+let a6 = a1.sorted(by: <)
+print(a6.description)
+
+
 
